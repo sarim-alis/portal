bfca96e46dea05a7c2855be5154efa59ab83ac86
sarim-alis <sarimslayerali786@gmail.com>
2025-07-31 21:55:05 +0500

    feat/Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Login from "./pages/Login.jsx";
+import Home from "./pages/Home.jsx";
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <div>
          <Login />
+         <Home />
       </div>
     </>
   )
