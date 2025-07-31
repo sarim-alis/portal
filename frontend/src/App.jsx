50d758c1ded8fa336f6cec67d6e14163dc986a7f
sarim-alis <sarimslayerali786@gmail.com>
2025-07-31 19:09:49 +0500

    feat/Login start

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
+import React from "react";
+import Login from "./Components/Login/Login";
+
 function App() {
 
   return (
     <>
       <div>
          <h1 className="text-3xl font-bold text-pink-500 m-4">Rasengan 🌬️</h1>
+         <Login />
       </div>
     </>
   )
