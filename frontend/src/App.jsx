4178fa9b56caef42afb147265ceaf6102d16a2f2
sarim-alis <sarimslayerali786@gmail.com>
2025-08-12 20:14:47 +0500

    feat/Toast setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import Login from "./pages/Login.jsx";
 import Home from "./pages/Home.jsx";
+import { ToastContainer, toast } from 'react-toastify';
 
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -14,6 +15,7 @@ function App() {
     <>
       <div>
         {isAuthenticated ? <Home /> : <Login onLogin={handleLogin} />}
+        <ToastContainer />
       </div>
     </>
   );
