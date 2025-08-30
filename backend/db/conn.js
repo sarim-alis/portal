d483f921920bcfc3ec6138ad54be8e43abc6d8a1
sarim-alis <sarimslayerali786@gmail.com>
2025-08-31 01:06:53 +0500

    feat/Setup for local

--- backend/db/conn.js
+++ backend/db/conn.js
@@ -8,10 +8,10 @@ dotenv.config();
 const { Pool } = pg;
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: {
-    require: true,
-    rejectUnauthorized: false,
-  },
+  // ssl: {
+  //   require: true,
+  //   rejectUnauthorized: false,
+  // },
 });
 
 export default pool;
